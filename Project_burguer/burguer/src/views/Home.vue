138051b7e48394b98edc6fdf65ae1dfc65be7dbc
Victor Soles <victorsoles09@gmail.com>
2022-04-12 23:41:14 -0300

    Adicionando Banner

--- Project_burguer/burguer/src/views/Home.vue
+++ Project_burguer/burguer/src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="home">
-    <h1>HOME</h1>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte seu Hamburguer:</h1>
+    <p>Formulário:</p>
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue"
+
+export default {
+  name: "Home",
+  components: {
+    Banner
+  }
+}
 
 </script>
