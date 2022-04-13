587ada354d1f8852d15594e735a266c137d0c058
Victor Soles <victorsoles09@gmail.com>
2022-04-13 00:30:34 -0300

    Adicionando Formulario

--- Project_burguer/burguer/src/views/Home.vue
+++ Project_burguer/burguer/src/views/Home.vue
@@ -2,17 +2,19 @@
   <Banner />
   <div class="main-container">
     <h1>Monte seu Hamburguer:</h1>
-    <p>Formulário:</p>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue"
+import BurgerForm from "../components/BurgerForm.vue"
 
 export default {
   name: "Home",
   components: {
-    Banner
+    Banner,
+    BurgerForm
   }
 }
 
